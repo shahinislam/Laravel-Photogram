1eb7e3f562885faef0304a4fcb93852f3b4e04af
Shahinur Islam <shahin.sec.cse@gmail.com>
2020-04-21 16:14:40 +0600

    first commit

---
+++ app/Profile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/Bsk7KeJihkFDHyXnGQiG3ad6IbcdUfdsihbQYMc3.png';
+
+        return '/storage/' . $imagePath;
+    }
+
+    public function user()
+    {
+       return $this->belongsTo(User::class);
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Welcome to PhotoGram
+
+This is a copy project of instagram. Coder tape author.
+
+@component('mail::button', ['url' => ''])
+    Confirm
+@endcomponent
+
+Shahinur Islam,<br>
+photogram.com
+@endcomponent
